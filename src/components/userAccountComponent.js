1931bfe29c46d8f6631374d4a3a47794a21320ea
devin-bielejec <devin.bieleje@gmail.com>
2019-08-26 14:41:45 -0400

    created some fake data

---
+++ src/components/userAccountComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+/* Username, password, image_url, bio */ 
+const fakeData = {username: "johnsmithiscool", password: "tacos", image_url: "https://images.unsplash.com/photo-1566781939916-4432f51a493a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80", bio: "I like turtles"}
+
+const UserAccountComponent = () => {
+    
+}
