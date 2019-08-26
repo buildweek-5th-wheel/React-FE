67ffcfd9639c84246c1c9a9fa578dd4d7d2d49de
devin-bielejec <devin.bieleje@gmail.com>
2019-08-26 17:22:26 -0400

    Added fake props to useraccountcomponent

--- src/components/userAccountComponent.js
+++ src/components/userAccountComponent.js
@@ -1,14 +1,15 @@
 import React from "react";
 import UserAccountForm from "../forms/userAccount";
 
-/* Username, password, image_url, bio */ 
-const fakeData = {username: "johnsmithiscool", password: "tacos", image_url: "https://images.unsplash.com/photo-1566781939916-4432f51a493a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80", bio: "I like turtles"}
 
 const UserAccountComponent = (props) => {
-    console.log(props);
+    
+    //TESTING - Changing props to fake data
+    props = {username: "johnsmithiscool", password: "tacos", image_url: "https://images.unsplash.com/photo-1566781939916-4432f51a493a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80", bio: "I like turtles"};
+
     return(
         <div>
-            <UserAccountForm username={"Devin"} password={"apwoeifjapoweifjpaoweifj"}/>
+            <UserAccountForm username={props.username} password={props.password} bio={props.bio}/>
         </div>
     )
 }
