2b878eef5411f7287b3bf733105f1d0dda2082d4
devin-bielejec <devin.bieleje@gmail.com>
2019-08-27 19:10:24 -0400

    Added styles to sign up page including a link to login if user already is signed up

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,27 +1,37 @@
 import React from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
+import { Link } from "react-router-dom";
 import "../../src/scss/formStyles.scss";
 import "../../src/scss/signUp.scss";
+
 
 function SignUpForm({ values, errors, touched, isSubmitting }) {
   return (
     <Form className="signUp">
       <div>
+        <h3>Sign Up!</h3>
         <p>User Name</p>
         {touched.username && errors.username && <p>{errors.username}</p>}
         <Field type="username" name="username" placeholder="username" />
       </div>
+
       <div>
         <p>Password</p>
         {touched.password && errors.password && <p>{errors.password}</p>}
         <Field type="password" name="password" placeholder="Password" />
       </div>
-      <label>
-        <Field type="checkbox" name="landowner" checked={values.landowner} />
-        Landowner
-      </label>
+
+      <div className="checkbox">
+        <p>Are you a land owner?</p>
+        <label>
+          <Field type="checkbox" name="landowner" checked={values.landowner} />
+          Landowner
+        </label>
+      </div>
+
       <button type="submit" disabled={isSubmitting}>Submit</button>
+      <Link to="/login">Already signed up? Click here to login</Link>
     </Form>
   );
 }
