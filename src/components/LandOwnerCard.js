f8a6008901234fb25cb179c0b8cb57c520c45701
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-08-27 14:26:05 -0400

    changed styles to listings

--- src/components/LandOwnerCard.js
+++ src/components/LandOwnerCard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {Card} from 'semantic-ui-react'
+
 
 
 //Land owner user page
@@ -11,12 +13,23 @@ const OwnerCard = (props) => {
 
         <div className="owner-card " key={props.listing_id}>
 
-
+<Card>
             <img src={props.owner.img_url} />
-            <h1>Name:{props.owner.listing_name}</h1>
-            <h3>Listing ID:{props.owner.listing_id}</h3>
-            <h3>User:{props.owner.user_id}</h3>
-            <h3>Description:{props.owner.description}</h3>
+
+        <h1>Name: {props.owner.listing_name}</h1>
+            
+            <h3>Listing ID: {props.owner.listing_id}</h3>
+            <h3>User: {props.owner.user_id}</h3>
+            <h3>Description: {props.owner.description}</h3>
+
+
+      </Card>         
+
+
+    
+    
+   
+
         </div>
     );
 }
