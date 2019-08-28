dde1591ef86c6669244aece29d373104643e85ce
devin-bielejec <devin.bieleje@gmail.com>
2019-08-28 19:11:45 -0400

    Bookings show up, not styled

--- src/components/BookingsPage.js
+++ src/components/BookingsPage.js
@@ -1,84 +1,46 @@
-// import React from "react";
-// import { Card, Icon, Image } from 'semantic-ui-react'
-// import "../scss/LandOwner.scss";
+import "../scss/Listings.scss";
+import React, { useEffect, useState } from "react";
+import { Card, Button, Header, Image, Modal } from 'semantic-ui-react'
 
-// //Land owner user page
+/* startDate, endDate, listing_name */
+const BookingsCard = props => {
+  return (
+    <Card key={props["listing_id"]}>
+      {/* <Image src={props.booking["img_url"]} wrapped ui={false} /> */}
+      <Card.Content>
+      <Card.Header>{props.listingName}</Card.Header>
+        <Card.Meta>
+        {/* <img src={logo-nav-white} alt=""/> */}
+        </Card.Meta>
+        <Card.Description>
+            <h4>Start Date: {props.startDate}</h4>
+            <h4>End Date: {props.endDate}</h4>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        {/* <button className="LandCard-Edit-Button">Edit (testing)</button> */}
+      </Card.Content>
+    </Card>
+  );
+  };
 
+export default function BookingsComponent() {
+    const data = [{"listing_id": 1, startDate: "Jan-01-2019", endDate: "Jan-11-2019", "listing_name": "Chicken Tacos"}, {"listing_id": 2, startDate: "Jan-01-2019", endDate: "Jan-11-2019", "listing_name": "Chicken Tacos"}, {"listing_id": 3, startDate: "Jan-01-2019", endDate: "Jan-11-2019", "listing_name": "Chicken Tacos"}];
 
-
-// const OwnerCard = props => {
-//   return (
-//     <Card key={props["listing_id"]}>
-//       <Image src={props.owner["img_url"]} wrapped ui={false} />
-//       <Card.Content>
-//       <Card.Header>{props.owner["listing_name"]}</Card.Header>
-//         <Card.Meta>
-//         {/* <img src={logo-nav-white} alt=""/> */}
-//         </Card.Meta>
-//         <Card.Description>
-//           {props.owner.description}
-//         </Card.Description>
-//       </Card.Content>
-//       <Card.Content extra>
-//         {/* <button className="LandCard-Edit-Button">Edit (testing)</button> */}
-//       </Card.Content>
-//     </Card>
-//   );
-//   };
-
-// export default OwnerCard;
-
-// // const OwnerCard = props => {
-// //   return (
-// //     <Card className="owner-card " key={props.listing_id}>
-// //       <img src={props.owner.img_url} />
-// //       <h1>Name: {props.owner.listing_name}</h1>
-// //       <h3>Listing ID: {props.owner.listing_id}</h3>
-// //       <h3>User: {props.owner.user_id}</h3>
-// //       <h3>Description: {props.owner.description}</h3>
-// //     </Card>
-// //   );
-// // };
-
-// // export default OwnerCard;
-
-
-
-
-
-// import React, { useEffect, useState } from "react";
-// import OwnerCard from "./LandOwnerCard.js"
-// import data from "./DummyData.js"
-// import { Button, Header, Image, Modal } from 'semantic-ui-react'
-// import LandImg from "../images/land1.jpg";
-// import AddListingForm from "../forms/addListingForm";
-// import "../scss/LandOwner.scss";
-
-// const AddListingButtonModal = (props) => (
-//     <Modal trigger={<Button className="add">Add</Button>}>
-//       <Modal.Header>Add a Listing</Modal.Header>
-//         <AddListingForm />
-//     </Modal>
-//   )
-
-// //Land owner user page
-
-// export default function OwnerListings() {
-
-//     const [OwnerList, setOwnerList] = useState(data);
-//     console.log(data);
-//     return (
-//         <div className="owner-list-parent">
-//             <h1>Your land listings:</h1>
-//             {AddListingButtonModal()}
-//             <section className="land-list-owner">
-//                 {OwnerList.map(owner => (
-//                 <OwnerCard
-//                     key={owner.listing_id}
-//                     owner={owner}
-//                 />
-//             ))}
-//             </section >
-//     </div>
-//     )
-//             }
+    console.log(data);
+    return (
+        <div className="booking-list-parent">
+            <h1>Your Bookings</h1>
+            <section className="land-list-booking">
+                {data.map(booking => (
+                <BookingsCard
+                    key={booking.listing_id}
+                   startDate={booking.startDate}
+                   endDate={booking.endDate}
+                   listingName={booking["listing_name"]}
+                />
+            ))}
+            </section >
+    </div>
+    )
+            }
