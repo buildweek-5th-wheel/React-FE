be7284ced97db3e51d6b1d97ae42ee3f64759b86
devin-bielejec <devin.bieleje@gmail.com>
2019-08-28 18:53:19 -0400

    semantic navbar installed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,39 +16,39 @@ const Navbar = () => {
   const token = localStorage.getItem("token");
 
   return (
-    <>
+    <> 
       {token ? (
         <Menu>
           <Menu.Item
-            name="listing"
-            active={active.activeItem === "listing"}
+            name="My Listings"
+            active={active.activeItem === "My Listings"}
+            onClick={handleClick}
+            as={Nav}
+            to={`/userListing`}
+          />
+          <Menu.Item
+            name="Marketplace"
+            active={active.activeItem === "Marketplace"}
             onClick={handleClick}
             as={Nav}
             to={`/listing`}
           />
+          <Menu.Item
+            name="Profile"
+            active={active.activeItem === "Profile"}
+            onClick={handleClick}
+            as={Nav}
+            to={`/profile`}
+          />
+        </Menu>
+      ) : (
+        <Menu>
           <Menu.Item
             name="RV spots"
             active={active.activeItem === "RV spots"}
             onClick={handleClick}
             as={Nav}
             to={`/`}
-          />
-          <Menu.Item
-            name="Account Settings"
-            active={active.activeItem === "Account Settings"}
-            onClick={handleClick}
-            as={Nav}
-            to={`/account`}
-          />
-        </Menu>
-      ) : (
-        <Menu>
-          <Menu.Item
-            name="Home"
-            active={active.activeItem === "home"}
-            onClick={handleClick}
-            as={Nav}
-            to={`/home`}
           />
           <Menu.Item
             name="Login"
