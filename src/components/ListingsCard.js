aac0e59e846b099712a93ec9192a3fc251e7bf68
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-08-29 01:31:21 -0400

    added styling to book form

--- src/components/ListingsCard.js
+++ src/components/ListingsCard.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { Card, Icon, Image, Modal, Button} from 'semantic-ui-react'
+import { Card, Icon, Image, Modal, Button } from 'semantic-ui-react'
 import "../scss/Listings.scss";
 import AddBookingForm from "../forms/BookForm";
 
-// import logo-nav-white from "..\images\logo-nav-white.png";
+import headerImg from "../images/headerImg.jpg";
 const AddListingButtonModal = (props) => (
-  <Modal trigger={<Button className="add">Book</Button>}>
+  <Modal trigger={<Button className="add-booking">Book</Button>}>
     <Modal.Header>Add a Booking</Modal.Header>
-      <AddBookingForm />
-     
+    <img className ="book-img" src={headerImg} alt="A van parked near a sunset"/>
+    <AddBookingForm />
+
   </Modal>
 )
 //All Land Listings
@@ -17,18 +18,18 @@ const LandCard = props => {
     <Card key={props["listing_id"]}>
       <Image src={props.land["img_url"]} wrapped ui={false} />
       <Card.Content>
-      <h2>Book today:</h2> 
+        <h2>Book today:</h2>
         <Card.Header>{props.land["listing_name"]}</Card.Header>
         <Card.Meta>
-        {/* <img src={logo-nav-white} alt=""/> */}
+        
         </Card.Meta>
         <Card.Description>
           {props.land.description}
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
-      {AddListingButtonModal()}
-        
+        {AddListingButtonModal()}
+
       </Card.Content>
     </Card>
   );
